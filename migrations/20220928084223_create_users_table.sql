24633e7c86b054d18de7ee40fa4697fdd62bc32a
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-28 16:08:54 +0700

    Update users table

--- migrations/20220928084223_create_users_table.sql
+++ migrations/20220928084223_create_users_table.sql
@@ -1,1 +1,10 @@
 -- Add migration script here
+CREATE TABLE users(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phonenumber TEXT NOT NULL,
+    email TEXT NOT NULL
+    password TEXT NOT NULL
+);
