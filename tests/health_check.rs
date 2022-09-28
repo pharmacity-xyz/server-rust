153294b8bf5c94c7ace5fc5f5c953047984edd6f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-28 08:00:10 +0700

    Rewrite test case

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,3 +4,23 @@ use pharmacity::main;
 fn dummy_test() {
     main()
 }
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app().await.expect("Failed to spawn app.");
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    todo!()
+}
