0d66630d0374a101fb60af49f9bf3ffe1d3bcac3
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-29 11:01:26 +0700

    Add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use pharmacity::configuration::get_configuration;
 use pharmacity::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
