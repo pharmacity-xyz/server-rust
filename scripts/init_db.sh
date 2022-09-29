1278c89281c8c66453195024dfdf0bbd6de3b91f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-29 09:45:36 +0700

    Update scripts

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)"]; then
+if ! [ -x "$(command -v psql)" ]; then
     echo >&2 "Error psql is not installed."
     echo >&2 "Use:"
     echo >&2 "sudo apt-get install postgresql-client"
@@ -26,7 +26,9 @@ DB_NAME="${POSTGRES_DB:=pharmacity-db}"
 # Check if a custom port has been set, otherwise default to 'pharmacity-db' 
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using Docker
+# Allow to skip Docker if a dockernized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
 docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -34,7 +36,7 @@ docker run \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
-    # ^Increased maximum number of connections for testing purpose
+fi
 
 # Keep pinging Posgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -43,7 +45,10 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c "\q";
     sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
