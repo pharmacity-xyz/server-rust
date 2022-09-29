8214607a7a5da23033f37a7cc943bcb90d603de1
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-29 10:30:43 +0700

    fmt

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
-use actix_web::{HttpServer, web, App};
-use actix_web::dev::Server;
 use crate::routes::health_check;
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
