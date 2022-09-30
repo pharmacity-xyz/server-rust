6ebf19fced5b648485de9886b62556c5e80d8ebb
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-30 22:43:11 +0700

    Renamed connection_pool

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use pharmacity::configuration::get_configuration;
 use pharmacity::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
