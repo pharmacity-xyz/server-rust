046e477f69c02ceb2112a35970f42886f68bd523
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-30 22:21:01 +0700

    Fix the issue

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,16 @@
-use crate::routes::health_check;
-use crate::routes::post_user;
+use crate::routes::{health_check, post_user};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/user", web::post().to(post_user))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
