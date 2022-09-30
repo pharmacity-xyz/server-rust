55aaf63f373ef14cfccaeca054e042ef6f614520
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-30 22:37:27 +0700

    Update

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct User {
@@ -9,7 +11,20 @@ pub struct User {
     password: String,
 }
 
-pub async fn post_user(_user: web::Json<User>) -> HttpResponse {
-
+pub async fn post_user(user: web::Json<User>, connection: web::Data<PgConnection>) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO users (id, name, address, phonenumber, email, password)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        "#,
+        Uuid::new_v4(),
+        user.name,
+        user.address,
+        user.phonenumber,
+        user.email,
+        user.password
+    )
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
