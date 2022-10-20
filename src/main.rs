b0f10730ab797d796df619c86c6f21323871a2a3
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-20 14:09:42 +0700

    Update main function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use pharmacity::configuration::get_configuration;
 use pharmacity::startup::run;
 use pharmacity::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,9 +12,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
