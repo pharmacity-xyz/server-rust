647d8b11994063e3847123b6ef399fca6ae8d278
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-22 08:54:19 +0700

    Handle salt

--- src/auth/password.rs
+++ src/auth/password.rs
@@ -1,6 +1,7 @@
 use actix_web::http::header::HeaderMap;
 use anyhow::Context;
-use secrecy::{Secret, ExposeSecret};
+use argon2::{Algorithm, Argon2, Params, Version, PasswordHasher};
+use secrecy::{ExposeSecret, Secret};
 use sha3::Digest;
 use sqlx::PgPool;
 
@@ -54,11 +55,14 @@ async fn get_stored_credentials(
     email: &str,
     pool: &PgPool,
 ) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
-    let row = sqlx::query!(r#"SELECT id, password_hash FROM users WHERE email = $1"#, email,)
-        .fetch_optional(pool)
-        .await
-        .context("Failed to performed a query to retrieve stored credentials")?
-        .map(|row| (row.id, Secret::new(row.password_hash)));
+    let row = sqlx::query!(
+        r#"SELECT id, password_hash FROM users WHERE email = $1"#,
+        email,
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to performed a query to retrieve stored credentials")?
+    .map(|row| (row.id, Secret::new(row.password_hash)));
     Ok(row)
 }
 
@@ -69,9 +73,13 @@ pub async fn validate_credentials(
     let mut user_id = None;
     let mut expected_password_hash = Secret::new(String::new());
 
-    let password_hash = sha3::Sha3_256::digest(
-        credentials.password.expose_secret().as_bytes()
+    let hasher = Argon2::new(
+        Algorithm::Argon2id,
+        Version::V0x13,
+        Params::new(15000, 2, 1, None).expect("Failed to create params"),
     );
+
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
     let password_hash = format!("{:x}", password_hash);
 
     if let Some((stored_user_id, stored_password_hash)) =
