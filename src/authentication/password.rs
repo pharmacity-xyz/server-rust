fb38c1b0edc020a7e6831ed2b70648f6eb2d4d74
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-23 09:43:25 +0700

    Change password

--- src/authentication/password.rs
+++ src/authentication/password.rs
@@ -1,6 +1,9 @@
 use actix_web::http::header::HeaderMap;
 use anyhow::Context;
-use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use argon2::{
+    password_hash::SaltString, Argon2, Params, PasswordHash, PasswordHasher, PasswordVerifier,
+    Version,
+};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
@@ -53,7 +56,7 @@ pub fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::
 }
 
 #[tracing::instrument(name = "Validate stored credentials", skip(email, pool))]
-async fn get_stored_credentials(
+pub async fn get_stored_credentials(
     email: &str,
     pool: &PgPool,
 ) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
@@ -117,4 +120,42 @@ fn verify_password_hash(
         )
         .context("Invalid password.")
         .map_err(AuthError::InvalidCredentials)
+}
+
+#[tracing::instrument(name = "Change password", skip(user_id, password, pool))]
+pub async fn change_password(
+    user_id: uuid::Uuid,
+    password: Secret<String>,
+    pool: &PgPool,
+) -> Result<(), anyhow::Error> {
+    let password_hash = spawn_blocking_with_tracing(move || compute_password_hash(password))
+        .await?
+        .context("Failed to hash password")?;
+    sqlx::query!(
+        r#"
+        UPDATE users
+        SET password_hash = $1
+        WHERE id = $2
+        "#,
+        password_hash.expose_secret(),
+        user_id
+    )
+    .execute(pool)
+    .await
+    .context("Failed to change user's password in the database")?;
+
+    Ok(())
+}
+
+fn compute_password_hash(password: Secret<String>) -> Result<Secret<String>, anyhow::Error> {
+    let salt = SaltString::generate(&mut rand::thread_rng());
+    let password_hash = Argon2::new(
+        argon2::Algorithm::Argon2id,
+        Version::V0x13,
+        Params::new(15000, 2, 1, None).unwrap(),
+    )
+    .hash_password(password.expose_secret().as_bytes(), &salt)?
+    .to_string();
+
+    Ok(Secret::new(password_hash))
 }
--- src/routes/auth/change_password.rs
+++ src/routes/auth/change_password.rs
@@ -1,10 +1,17 @@
+use crate::authentication::get_stored_credentials;
 use actix_web::{web, HttpResponse};
+use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
-use actix_web_flash_messages::FlashMessage;
+
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    util::{e500, see_other},
+};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
+    email: String,
     current_password: Secret<String>,
     new_password: Secret<String>,
     new_password_check: Secret<String>,
@@ -20,5 +27,25 @@ pub async fn change_password(
         )
         .send();
     }
-    todo!()
+    let credentials = Credentials {
+        email: form.email.clone(),
+        password: form.current_password.clone(),
+    };
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Err(e500(e))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e)),
+        };
+    }
+    let user_id = sqlx::query!(r#"SELECT id FROM users WHERE email = $1"#, email,)
+        .fetch_optional(pool)
+        .await?;
+    crate::authentication::change_password(user_id.expect(""), form.new_password, &pool)
+        .await
+        .map_err(e500)?;
+    FlashMessage::error("Your password has been changed.").send();
+    Ok(HttpResponse::Ok().finish())
 }
