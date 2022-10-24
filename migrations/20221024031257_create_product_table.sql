6f13675e2c753726d15364c9cb134241a80a80f9
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-24 10:27:32 +0700

    Create table for product

---
+++ migrations/20221024031257_create_product_table.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+CREATE TABLE products(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    stock INT NOT NULL,
+    price INT NOT NULL,
+    category_id uuid NOT NULL,
+    CONSTRAINT fk_category
+        FOREIGN KEY(category_id)
+            REFERENCEs categories(id)
+);
