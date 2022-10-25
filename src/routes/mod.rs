4a1f9d77ba2ce0d9687568142dfc0492484cf226
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-25 13:50:26 +0700

    Move

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,14 +2,14 @@ pub mod auth;
 pub mod carts;
 pub mod categories;
 pub mod health_check;
+pub mod order;
 pub mod products;
 pub mod users;
-pub mod order;
 
 pub use auth::*;
 pub use carts::*;
 pub use categories::*;
 pub use health_check::*;
+pub use order::*;
 pub use products::*;
 pub use users::*;
-pub use order::*;
--- src/routes/order/post.rs
+++ src/routes/order/post.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, HttpResponse, ResponseError};
 use sqlx::PgPool;
 use stripe::{
-    Client, CreateCustomer, CreatePrice, CreateProduct, Currency, Customer, IdOrCreate, Product,
+    CheckoutSession, Client, CreateCheckoutSession, CreateCheckoutSessionLineItems, CreateCustomer,
+    CreatePrice, CreateProduct, Currency, Customer, Expandable, IdOrCreate, Price, Product,
 };
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -14,9 +15,9 @@ pub struct RequestProduct {
     category_id: uuid::Uuid,
 }
 
-pub async fn post_product(
-    product: web::Json<RequestProduct>,
-    pool: web::Data<PgPool>,
+pub async fn post_order(
+    _product: web::Json<RequestProduct>,
+    _pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, PostProductError> {
     let secret_key = std::env::var("STRIPE_SECRET_KEY").expect("Missing STRIPE_SECRET_KEY in env");
     let client = Client::new(secret_key);
@@ -72,22 +73,53 @@ pub async fn post_product(
         Price::create(&client, create_price).await.unwrap()
     };
 
-    sqlx::query!(
-        r#"
-        INSERT INTO products (id, name, description, image_url, stock, price, category_id, featured)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, false)
-        "#,
-        uuid::Uuid::new_v4(),
-        product.name,
-        product.description,
-        product.image_url,
-        product.stock,
-        product.price,
-        product.category_id,
-    )
-    .execute(pool.get_ref())
-    .await
-    .map_err(PostProductError)?;
+    println!(
+        "created a product {:?} at price {} {}",
+        product.name.unwrap(),
+        price.unit_amount.unwrap() / 100,
+        price.currency.unwrap()
+    );
+
+    let checkout_session = {
+        let mut params =
+            CreateCheckoutSession::new("http://test.com/cancel", "http://test.com/success");
+        params.customer = Some(customer.id);
+        params.mode = Some(stripe::CheckoutSessionMode::Payment);
+        params.line_items = Some(vec![CreateCheckoutSessionLineItems {
+            quantity: Some(3),
+            price: Some(price.id.to_string()),
+            ..Default::default()
+        }]);
+        params.expand = &["line_items", "line_items.data.price.product"];
+
+        CheckoutSession::create(&client, params).await.unwrap()
+    };
+
+    println!(
+        "created a {} checkout session for {} {:?} for {} {} at {}",
+        checkout_session.payment_status,
+        checkout_session.line_items.data[0].quantity.unwrap(),
+        match checkout_session.line_items.data[0]
+            .price
+            .as_ref()
+            .unwrap()
+            .product
+            .as_ref()
+            .unwrap()
+        {
+            Expandable::Object(p) => p.name.as_ref().unwrap(),
+            _ => panic!("product not found"),
+        },
+        checkout_session.amount_subtotal.unwrap() / 100,
+        checkout_session.line_items.data[0]
+            .price
+            .as_ref()
+            .unwrap()
+            .currency
+            .unwrap(),
+        checkout_session.url.unwrap()
+    );
+
     Ok(HttpResponse::Ok().finish())
 }
 
