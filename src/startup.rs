ca909275c91b9a1e2af220136ed418f79f5963de
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-16 23:00:20 +0700

    Add new origin

--- src/startup.rs
+++ src/startup.rs
@@ -76,6 +76,7 @@ async fn run(
             .wrap(
                 Cors::default()
                     .allowed_origin("http://localhost:3000")
+                    .allowed_origin("https://ui-ovno40cdf-pharmacity-xyz.vercel.app")
                     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
